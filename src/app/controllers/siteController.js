5ca0661106b8de860ab7e03563face10c3510859
PhanhotboY <phanhotboy@DESKTOP-8VPG2GT>
2023-03-05 15:42:30 +0700

    restructure project folder - code has not been changed yet

--- src/app/controllers/siteController.js
+++
@@ -1,9 +0,0 @@
-const { sequelize } = require('../../config/db');
-
-const siteController = {
-    home(req, res, next) {
-        return res.render('products');
-    },
-};
-
-module.exports = siteController;
